7ac5f3ba23dea53fd1aa0d6cc57a7a841229a80a
Shubham5698 <167434891+Shubham5698@users.noreply.github.com>
2024-05-16 14:38:15 +0530

    Update File.java

--- File.java
+++ File.java
@@ -1,10 +1,12 @@
 import java.lang.*;
 
 public class Main{
-   
+
+   int x=1000;
 public static void main(String [] args)
 {
   System.out.println("Hello");
   System.out.println("Shubham");
+   
 }
 }
