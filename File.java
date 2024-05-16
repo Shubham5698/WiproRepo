1404a0b8c9a2de33bbe95ea1e6cf9b34c3b665fd
Shubham5698 <shubhampore5698@gmail.com>
2024-05-17 00:07:12 +0530

    Added one method in File.java

--- File.java
+++ File.java
@@ -3,6 +3,10 @@ import java.lang.*;
 public class Main{
 
    int x=1000;
+  public int add()
+  {
+	  System.out.println("This is addition method");
+  }
 public static void main(String [] args)
 {
   System.out.println("Hello");
